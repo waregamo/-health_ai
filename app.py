5f30d40ef807c06078f6247536827df47cb801a7
waregamo <waregamoses20@gmail.com>
2025-07-20 19:41:49 +0300

    Add disease explanation after image upload

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ def authenticate():
             .login-title {
                 font-size: 28px;
                 font-weight: 600;
-                color:#006400;
+                color:#008000;
             }
         </style>
         """, unsafe_allow_html=True)
